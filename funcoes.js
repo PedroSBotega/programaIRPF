b01f165d65436d4796294162b13b5fd9ab28ba62
PedroSBotega <85083659+PedroSBotega@users.noreply.github.com>
2022-04-12 17:32:39 -0300

    Propriedades e Array sem duplicatas
    Alteração do typeof das propriedades das lista de negociacoes e criando array com a lista de todas as acoes negociadas (sem duplicatas)

--- funcoes.js
+++ funcoes.js
@@ -4,23 +4,35 @@
 const criarListaNegociacao = (negociacoes) => {
 
     let listaNegociacao = [];
-
+    let i = 1;
     for (let linha of negociacoes)
     {
         let linhaNegociacao = linha.split('	');
-        let negociacao = {                              //A posição 0 do array recebe um espaço em branco(ao copiar os dados da tabela excel 
+        let negociacao = {       
+            posicao: i,                                 //A posição 0 do array recebe um espaço em branco(ao copiar os dados da tabela excel 
             data: linhaNegociacao[1].trim(),            //esse 'espaço' indica a divisão entre colunas), portanto utiliza-se da posição 1 em diante
             operacao: linhaNegociacao[2].trim(),        //Os dados copiados diretamente do excel tinham alguns espaços em branco nas células
             acao: linhaNegociacao[3].trim(),            //para reduzir ao máximo as operaçoes previas no excel, utiliza-se a função .trim()
-            qtd: linhaNegociacao[4].trim(),
-            pmOperacao: linhaNegociacao[5].trim(),
-            total: linhaNegociacao[6].trim()
+            qtd: parseInt(linhaNegociacao[4].trim()),
+            pmOperacao: parseFloat(linhaNegociacao[5].trim().replace(",",".")),
+            total: parseFloat(linhaNegociacao[6].trim().replace(",","."))
         }
+        i += 1;
         listaNegociacao.push(negociacao);
     }
     return listaNegociacao
 }
 
+//Utilizado para criar uma array apenas com os códigos das ações negociadas
+
+const apenasCodigo = (lista) => {
+    return lista.acao
+}
+
+
+
+
+export {criarListaNegociacao, apenasCodigo};
 // Próximos passos
 // 1- conseguir identificar todos as ações compradas e criar um vetor com elas sem duplicatas
 // 2- conseguir calcular o PM 
@@ -38,4 +50,3 @@ const criarListaNegociacao = (negociacoes) => {
 
 
 
-export default criarListaNegociacao
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import negociacoes from "./dados.js";
-import criarListaNegociacao from "./funcoes.js";
+import {criarListaNegociacao, apenasCodigo} from "./funcoes.js";
 
 var listaNegociacao = criarListaNegociacao(negociacoes); 
 console.log(listaNegociacao);
-// Está feito at
+
+var listaAcoes = listaNegociacao.map(apenasCodigo); // Array com o código de todas as negociações, necessário remover duplicatas.
+console.log(listaAcoes);
+
+var listaAcoesUnicas = [ ... new Set(listaAcoes)]; // Remove duplicatas
+console.log(listaAcoesUnicas);
+
+
